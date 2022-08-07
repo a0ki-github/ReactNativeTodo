7bc17fababf5f836fcf9242a72396cfa94556694
a0ki-github <aoki.hotaka@gmail.com>
2022-08-07 13:48:14 +0900

    ref: format

--- App.js
+++ App.js
@@ -10,15 +10,18 @@ import {
 } from 'react-native';
 
 const todos = [
-  { name: 'clean room', key: 1 },
-  { name: 'trash garbage', key: 2 },
-  { name: 'go shopping', key: 3 },
+  { name: 'Clean room', key: 1 },
+  { name: 'Trash garbage', key: 2 },
+  { name: 'Go shopping', key: 3 },
 ];
 
 export default function App() {
   const renderItem = ({item}) => {
     return (
-      <Text>{ item.name }</Text>
+      <View style={ styles.item }>
+        <Text style={ styles.itemName }>{ item.name }</Text>
+        <Text>Delete</Text>
+      </View>
     )
   }
 
@@ -43,5 +46,16 @@ const styles = StyleSheet.create({
     fontSize: 40,
     fontWeight: "bold",
     marginTop: 80,
+  },
+  item: {
+    width: 300,
+    marginBottom: 30,
+    alignItems: 'center',
+    padding: 10,
+  },
+  itemName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
   }
 });
